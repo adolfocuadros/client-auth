c93a2539637cf5e8f295448a2b13959e3ac20474
Adolfo Cuadros <ronnie.adolfo@gmail.com>
2017-01-02 11:12:04 -0500

    ACL local

--- src/ACL.php
+++ src/ACL.php
@@ -10,27 +10,39 @@ use Illuminate\Http\Request;
 class ACL
 {
     public static function can(Request $request, $permission) {
-        if(!$request->hasHeader('Auth-Token')) {
-            return false;
-        }
         if(empty(config('renqo_client_acl.renqo_acl'))) {
             throw new ConfigException('Hay un problema con la configuración api_auth');
         }
-        try {
-            $client = new Client([
-                'base_uri' =>  config('renqo_client_acl.renqo_acl'),
-                'timeout'  => 15.0,
-                'headers'  => [
-                    'Auth-Token' => $request->header('Auth-Token')
-                ]
-            ]);
+        $configSession = config('renqo_client_acl.driver','cloud');
+        if($configSession == 'session') {
 
-            $client->request('POST', 'acl',[
-                'form_params'    =>  ['permission'=>$permission]
-            ]);
-        } catch (\Exception $e) {
-            return false;;
+            $usuario_permisos = \Auth::user()->permisos;
+
+            if(permission($usuario_permisos, $permission)) {
+                return true;
+            }
+        } else {
+
+            if(!$request->hasHeader('Auth-Token')) {
+                return false;
+            }
+
+            try {
+                $client = new Client([
+                    'base_uri' =>  config('renqo_client_acl.renqo_acl'),
+                    'timeout'  => 20.0,
+                    'headers'  => [
+                        'Auth-Token' => $request->header('Auth-Token')
+                    ]
+                ]);
+
+                $client->request('POST', 'acl',[
+                    'form_params'    =>  ['permission'=>$permission]
+                ]);
+            } catch (\Exception $e) {
+                return false;
+            }
         }
-        return true;
+        return false;
     }
 }
