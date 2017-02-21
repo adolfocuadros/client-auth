757e414e8e78c794315777d3976932a100638884
Adolfo Cuadros <ronnie.adolfo@gmail.com>
2017-02-21 10:25:53 -0500

    Mejora del metodo can sin necesidad de pasar el request

--- src/ACL.php
+++ src/ACL.php
@@ -9,7 +9,25 @@ use Illuminate\Http\Request;
 
 class ACL
 {
-    public static function can(Request $request, $permission) {
+    public static function can() {
+        $args = func_get_args();
+        foreach ($args as $arg) {
+            if($arg instanceof Request) {
+                $request = $arg;
+            }
+            if(is_string($arg)) {
+                $permission = $arg;
+            }
+        }
+
+        if(!isset($permission)) {
+            return false;
+        }
+
+        if(!isset($request)) {
+            $request = request();
+        }
+
         if(empty(config('renqo_client_acl.renqo_acl'))) {
             throw new ConfigException('Hay un problema con la configuración api_auth');
         }
